3656eb7befeb5d60901985fd287078bd97dd6f6b
kkosuke <kkosuke007@gmail.com>
2023-03-19 21:52:25 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen font-mono">
+      Hello Nextjs
+    </div>
+  );
+};
+export default Home;
