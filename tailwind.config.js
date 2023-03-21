9ed5be1e4114a870fdd0b9205b55ba0921b8da9d
kkosuke <kkosuke007@gmail.com>
2023-03-21 15:29:02 +0900

    ログイン

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false,
   theme: {
-      extend: {},
+    extend: {},
   },
   variants: {
-      extend: {},
+    extend: {
+      opacity: ['disabled'],
+    },
   },
   plugins: [],
-}
+};
